5b303d0d51cb5448fb2917ffd1bd5122cbf3767a
thedoris <theo.dermitzakis@outlook.com>
2025-09-17 03:10:38 +0100

    Added function for user cleanup

--- main.py
+++ main.py
@@ -1,54 +1,28 @@
 """
-Key concepts:
-
-1. The API endpoints syntax
-2. The decorators: Add some extra funtionality to the code, without relating directly to the function itself: 
-   Example: def repeat_decorator(times):
-                def decorator(func):
-                        def wrapper(name):
-                        for _ in range(times):
-                                print("Before the function call") #This is the important part
-                                result = func(name)
-                                print("After the function call")  #This is the important part
-                        return result
-                        return wrapper
-                return decorator
-
-3. Dependency Injection: a) Eliminates Global State Problems -> each user has its own calculator
-                         b) Better testability as before I had a global calculator that was being used by other tests
-                         c) Better resource management: Before  -> Global calculator lived in memory for ever
-                                                        With DI -> Controlled instance lifetime (per request)
-                         d) Scalability, each request gets its own isolated calculator instance 
-                         d) Explicit dependencies: Before I had a hidden gloab dependency, with DI it's clear what each endpoint requires
-                         e) Better error handling: With DI you can add dependency-specific error handling 
-
-                         
-QUESTIONS:
-1. Doesn't working with DI mean that I will need to use async functions since we are serving multiple users now concurrentlly?                         
-
-ISSUES:
-1. With DI and when you create a new instance for each user -> this creates problem with my current calculator. Keep it simple for now.
-
 TO DO:
 1. Difference between errors and exceptions -> see which is best suited
-2. Do it for multiple users 
-3. See a bit better the testing, part
+2. See a bit better the testing, part
+3. Test the app and read about async more - do I need to handle tasks when the app shutsdown?
 
 """
 
 
-from fastapi import FastAPI, HTTPException,status, Request
+from fastapi import FastAPI, HTTPException,status, Request, Response
 from typing import Dict, List, Optional
 from pydantic import BaseModel
 from calculator import Calculator
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta
+import uuid
+import asyncio
+from contextlib import asynccontextmanager
 
-
+@asynccontextmanager
+async def cleanup_users_task(app: FastAPI):
+        asyncio.create_task(user_cleanup())
+        yield
 
 app = FastAPI()
-calculator = Calculator()
-current_value: float = 0.0
 
 @dataclass
 class Action():
@@ -57,16 +31,39 @@ class Action():
         details: str
         timestamp: str
 
+@dataclass
+class User():
+        uid: str
+        calculator: Calculator  # Associated calculator
+        creation_date : datetime 
+        exp_date: int           # Associatted cookie expiry date
+        actions: List[Action]
 
-action_history: Dict[str, List[Action]] = {}
-users_list: Dict[str, Calculator] = {}
+user_list: Dict[str,User] = {}
 
-def track_action(ip: str, value: str, action: str):
+
+
+async def user_cleanup():
+        while True:
+                await asyncio.sleep(3*60)       # Run every 3 minutes
+
+                current_time = datetime.now()
+                cleanups_counter = 0
+
+                expired_users = [                       #It is more secure, timedelta handles some error prone things automatically, time zones, leap seconds etc.
+                        uid for uid, user in user_list.items()          
+                        if current_time > user.creation_date + timedelta(seconds=user.exp_date)         
+                ]
+
+                for uid in expired_users:
+                        del user_list[uid]
+                        cleanups_counter += 1
+                if cleanups_counter > 0:
+                        print(f"Cleaned up {cleanups_counter} expired users. Total users: {len(user_list)}")
+
+def track_action(user: User, value: str, action: str):
         try: 
-                if ip not in users_list:
-                        users_list[ip] = Calculator()
-                        action_history[ip] = []
-                calculator = users_list[ip]
+                calculator = user.calculator
                 initial_value = calculator.getCurrentValue()
                 
                 if action == "get_current_value":
@@ -98,21 +95,48 @@ def track_action(ip: str, value: str, action: str):
                         timestamp=datetime.now().isoformat()
                 )
 
-                action_history[ip].append(new_action)
+                user.actions.append(new_action)
 
                 # Keep up to 50 actions
-                if len(action_history[ip]) > 50:
-                        action_history[ip].pop(0)
+                if len(user.actions) > 50:
+                        user.actions.pop(0)
                 
                 return result
         except (ValueError, OverflowError) as e:
                 raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
-def get_IP(req: Request):
-        client_host = req.headers.get(
-            "x-forwarded-for",
-            req.client.host if req.client else "unknown"
-        )
-        return client_host
+
+
+def get_user(req: Request, response: Response):
+        uid = req.cookies.get("user_id")
+
+        if not uid:
+                uid = str(uuid.uuid4())
+                cookie_expiry_date = 10*60 
+
+                # Create the cookie
+                response.set_cookie(
+                        key="user_id",
+                        value=uid,
+                        max_age=cookie_expiry_date,  
+                        httponly=True,
+                        samesite="lax"
+                )
+
+                #Create new user
+                new_user = User(
+                        uid = uid,
+                        calculator= Calculator(),
+                        creation_date = datetime.now(),
+                        exp_date= cookie_expiry_date,
+                        actions= []
+                )
+                user_list[uid] = new_user
+        return user_list[uid]
+
+
+
+
+
 
 @app.get("/")
 def root() -> Dict[str, str]:
@@ -120,42 +144,46 @@ def root() -> Dict[str, str]:
 
 
 @app.get("/currentValue")
-def currentValue(req: Request):
-        ip = get_IP(req)
-        return track_action(ip,"None", "get_current_value")
+def currentValue(req: Request, response: Response):
+        user = get_user(req, response)
+        return track_action(user,"None", "get_current_value")
 
 @app.get("/add/{num}")
-def add(num:float, req: Request ):
-        return track_action(get_IP(req), str(num), "add")
+def add(num:float, req: Request, response: Response ):
+        user = get_user(req, response)
+        return track_action(user, str(num), "add")
 
 @app.get("/sub/{num}")
-def sub(num:float, req:Request ):
-        return track_action(get_IP(req), str(num), "subtract")
+def sub(num:float, req:Request, response: Response ):
+        user = get_user(req, response)
+        return track_action(user, str(num), "subtract")
 
 @app.get("/multiply/{num}")
-def multiply(num:float, req: Request ):
-        return track_action(get_IP(req), str(num),"multiply")
+def multiply(num:float, req: Request, response: Response ):
+        user = get_user(req, response)
+        return track_action(user, str(num),"multiply")
 
 @app.get("/divide/{num}")
-def divide(num:float, req:Request):
-        return track_action(get_IP(req), str(num), "divide")
+def divide(num:float, req:Request, response: Response):
+        user = get_user(req, response)
+        return track_action(user, str(num), "divide")
 
 @app.get("/clear")
-def clear(req: Request):
-        return track_action(get_IP(req),"None", "clear_output")
+def clear(req: Request, response: Response):
+        user = get_user(req, response)
+        return track_action(user,"None", "clear_output")
 
 @app.get("/my_action_history")
-def get_action_history(req: Request):
-        ip = get_IP(req)
-        print("Testing:", ip)
-        if ip not in action_history:
-                return {"Message": "No history was found for your ip"}
+def get_action_history(req: Request, response: Response):
+        user = get_user(req, response)
+        if len(user.actions) == 0:
+                return {"Message": "No history was found for your user"}
         
         return {
-                        "ip": ip,
-                        "actions": [action for action in action_history[ip]]
+                        "user_id": user.uid,
+                        "actions": user.actions
                 }
-                
+               
 
 if __name__ == "__main__":
         import uvicorn
